9d125081211b9f34ba68298bbd08c45cda5444a9
rafifprayata <opetrafif@gmail.com>
2025-01-13 12:07:05 +0700

    menambahkan koneksi

---
+++ IdeKreatif-starter-main/config.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "idekreatif";
+
+$conn = mysqli_connect($host, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("DATABASE GAGAL TERKONEKSI: " . $conn->connect_error);
+}
+?>

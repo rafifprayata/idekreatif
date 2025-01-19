584cf78048b1de68bb77dd6e8eca83e55d2650ea
rafifprayata <opetrafif@gmail.com>
2025-01-19 17:02:18 +0700

    header.php

--- IdeKreatif-starter-main/auth/.layouts/header.php
+++ IdeKreatif-starter-main/auth/.layouts/header.php
@@ -1,3 +1,15 @@
+<?php
+session_start(); // inisialisali session
+// ambil notifikasi jika ada, kemudian hapus dari sesi
+$notification = $_SESSION['notification'] ?? null;
+if ($notification) {
+  unset($_SESSION['notification']);
+}
+if (isset($_SESSION["username"]) || isset($_SESSION["role"])) {
+  $_SESSION['notification'] = ['type', => 'danger', 'message' => 'Silahkan Logout Terlebih Dahulu!'];
+  header('location: ../dashboard.php');
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
